15f6ce0e3072081e8e82d03fd0d1ac42c416cdb3
Pablo Munoz <pfariasmunoz@gmail.com>
2017-10-19 18:15:36 -0300

    Add viewmovel for licenses list

---
+++ app/src/main/java/com/pfariasmunoz/mymedicallicenses/licenseslist/LicenseListViewModel.java
@@ -0,0 +1,16 @@
+package com.pfariasmunoz.mymedicallicenses.licenseslist;
+
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+
+/**
+ * Created by Pablo Farias on 19-10-17.
+ */
+
+public class LicenseListViewModel extends AndroidViewModel {
+
+
+    public LicenseListViewModel(Application application) {
+        super(application);
+    }
+}
--- app/src/main/java/com/pfariasmunoz/mymedicallicenses/licenseslist/MainActivity.java
+++ app/src/main/java/com/pfariasmunoz/mymedicallicenses/licenseslist/MainActivity.java
@@ -1,7 +1,9 @@
-package com.pfariasmunoz.mymedicallicenses;
+package com.pfariasmunoz.mymedicallicenses.licenseslist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.pfariasmunoz.mymedicallicenses.R;
 
 public class MainActivity extends AppCompatActivity {
 
